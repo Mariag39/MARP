08693250030cbc4dcac39170972039dc107d4570
Maria Raldua <maria.raldua@gmail.com>
2019-09-17 02:27:43 +0200

    Add files via upload

---
+++ ucm.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <queue>
+using namespace std;
+//O(Nlog N + K log N), donde N es el número de
+//usuarios y K el número de consultas
+struct usuario {
+	int id;
+	int frec;
+	int momento;
+};
+
+bool operator<(usuario const& a, usuario const& b) {
+	return a.momento > b.momento ||
+		(a.momento == b.momento && a.id > b.id);
+}
+
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int casos;
+	cin >> casos;
+	if (casos == 0) {
+		cout << "---" << endl;
+		return false;
+	}
+	
+		priority_queue<usuario> cola;
+		for (int i = 0; i < casos; ++i) {
+			int id;
+			int frec;
+			cin >> id >> frec;
+			cola.push({ id,frec,frec});
+
+		}
+		int llama;
+		cin >> llama;
+		while(llama--) {
+			usuario p = cola.top();
+			cola.pop();
+			cout << p.id << endl;
+			p.momento += p.frec;
+			cola.push(p);
+		}
+
+
+	return true;
+
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ urgencias.cpp
@@ -0,0 +1,81 @@
+﻿// María García		
+// MARP17
+
+//O(Nlog N + K log N), donde N es el número de
+//pacientes y K el número de eventos
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "PriorityQueue.h"
+
+using namespace std;
+
+struct paciente {
+	string nombre;
+	long long int prioridad;
+	long long int gravedad;
+};
+
+bool operator<(const paciente &a, const paciente &b) {
+	return (a.gravedad > b.gravedad || (a.gravedad == b.gravedad && a.prioridad < b.prioridad));
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	long long int n = 0;
+	long long int cont = 0;
+	cin >> n;
+	PriorityQueue<paciente> p;
+	if (!std::cin)
+		return false;
+	for (long int i = 0; i < n; i++) {
+		char x;
+		cin >> x;
+		if (x == 'I') {
+			string nombre;
+			cin >> nombre;
+			long long int grav;
+			cin >> grav;
+			cont++;
+			p.push({ nombre,cont, grav });
+		}
+		else if (x == 'A') {
+			paciente pac = p.top();
+			cout << pac.nombre << endl;
+			p.pop();
+				
+		}
+			
+	}
+	if (n == 0) {
+		cout << "---" << endl;
+	}
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
