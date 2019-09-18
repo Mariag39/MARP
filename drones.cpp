3a337b93cbe26354d9fd1d53198c38a53a4502c5
Maria Raldua <maria.raldua@gmail.com>
2019-09-18 18:11:11 +0200

    Update drones.cpp

--- drones.cpp
+++ drones.cpp
@@ -1,5 +1,5 @@
-// Nombre del alumno .....
-// Usuario del Juez ......
+// Maria Garcia
+// MARP17
 
 
 #include <iostream>
@@ -7,82 +7,92 @@
 #include <fstream>
 #include <queue>
 #include <vector>
-
 using namespace std;
 
 
 
 bool resuelveCaso() {
-	long int n, a, b;
+	int n, a, b;
 	cin >> n >> a >> b;
-	if (!cin && n == 0 && a == 0 && b == 0) {
+	if (!cin || n < 1 || a < 1 || b < 1) {
 		return false;
 	}
-	priority_queue<int> nueve;
-	priority_queue<int> uno_medio;
-	for (int i = 0; i < a; i++) {
-		int nums;
-		cin >> nums;
-		nueve.push(nums);
-	}
-	for (int i = 0; i < b; i++) {
-		int nn;
-		cin >> nn;
-		uno_medio.push(nn);
-	}
-	vector<int> res;
-	int primn, primuno;
-	int i = 0;
-	int minimo = 0;
-	while(!nueve.empty() && !uno_medio.empty()){
-		for (int i = 0; i < n; i++) {
-			primn = nueve.top();
-			primuno = uno_medio.top();
-			nueve.pop();
-			uno_medio.pop();
-			minimo += min(primn, primuno);
-			if (primn < primuno) {
-				primuno -= primn;
-				uno_medio.push(primuno);
-			}
-			else if (primn > primuno) {
-				primn -= primuno;
-				nueve.push(primn);
-			}
-			if (nueve.empty() || uno_medio.empty()) break;
+	else {
+		priority_queue<int> nueve;
+		priority_queue<int> uno_medio;
+		for (int i = 0; i < a; i++) {
+			int nums;
+			cin >> nums;
+			nueve.push(nums);
 		}
-		res.push_back(minimo);
-		minimo = 0;
+		for (int i = 0; i < b; i++) {
+			int nn;
+			cin >> nn;
+			uno_medio.push(nn);
+		}
+		vector<int> res, v9, v1;
+		int primn, primuno;
+		int i = 0;
+
+		while (nueve.size() > 0 && uno_medio.size() > 0) {
+			int minimo = 0;
+			for (int i = 0; i < n; i++) {
+				primn = nueve.top(); nueve.pop();
+				primuno = uno_medio.top(); uno_medio.pop();
+
+				minimo += min(primn, primuno);
+
+				if (primn < primuno) {
+					primuno -= primn;
+					v1.push_back(primuno);
+					//uno_medio.push(primuno);
+				}
+				else if (primn > primuno) {
+					primn -= primuno;
+					v9.push_back(primn);
+					//nueve.push(primn);
+				}
+				if (nueve.size() == 0 || uno_medio.size() == 0) break;
+			}
+			for (unsigned int i = 0; i < v1.size(); i++) uno_medio.push(v1[i]);
+			for (unsigned int i = 0; i < v9.size(); i++) nueve.push(v9[i]);
+			v1.clear(); v9.clear();
+			res.push_back(minimo);
+
+		}
+
+		for (unsigned int i = 0; i < res.size(); i++) {
+			if (i == 0) cout << res[i];
+			else {
+				cout << " ";
+				cout << res[i];
+			}
+		}
+		cout << '\n';
 	}
-	
-	for (int i = 0; i < res.size(); i++) {
-		cout << res[i];
-		cout << " ";
-	}
-	cout << endl;
-	
 	return true;
+	
 
 }
 
 int main() {
 	// Para la entrada por fichero.
 	// Comentar para acepta el reto
-//#ifndef DOMJUDGE
-//	std::ifstream in("datos.txt");
-//	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-//#endif 
-//
-//
-	while (resuelveCaso())
+#ifndef domjudge
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso()) {}
 		;
-//
-//
-//	// Para restablecer entrada. Comentar para acepta el reto
-//#ifndef DOMJUDGE // para dejar todo como estaba al principio
-//	std::cin.rdbuf(cinbuf);
-//	system("PAUSE");
-//#endif
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
 
 	return 0;
 }
